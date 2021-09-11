29a24bc3682084bed92da3c5f854a1ef591ba5a9
Kiku2004 <74317831+Kiku2004@users.noreply.github.com>
2021-09-11 17:16:29 -0300

    Create 21.py

---
+++ 21.py
@@ -0,0 +1,28 @@
+    print("-//-//-//-//-//-//-//-//-//-//-//")
+    print(" Bem vindo ao jogo de BlackJack! ")
+    print("-//-//-//-//-//-//-//-//-//-//-//")
+
+
+    baralho = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
+    card_possible = random.randrange(0, baralho)
+    dealer_cards = []
+    player_cards = []
+
+
+    lost = False
+    hit = False
+    stand = False
+
+    while(not lost):
+
+        escolha = input("Hit or Stand:".lower())
+        escolha = escolha.strip()
+
+        if(hit == True):
+            player_cards.append(card_possible)
+            print("You have ", player_cards)
+        elif(stand == True):
+            while len(dealer_cards) != 2:
+                dealer_cards.append(card_possible)
+                if len(dealer_cards) == 2:
+                    print("Dealer has: _ and", dealer_cards[1])
