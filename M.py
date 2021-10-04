0f5b1a0f755ed8949553abe3b8acf78f6111acbb
Matheus Kikumori Almeida Leite <74317831+Kiku2004@users.noreply.github.com>
2021-10-04 14:50:58 -0300

    Update M.py

--- M.py
+++ M.py
@@ -2,32 +2,21 @@ import random
 
     print("--x-- Test tests --x--")
     
-    arquivo = open("respostas.txt", "r")
-    respostas = []
+
+    perguntas = ["pergunta1", "pergunta2", "pergunta3", "pergunta3", "pergunta4", "pergunta5"]
+
+    numero = random.randrange(0, len(perguntas))
+    pergunta_secreta = perguntas[numero].upper()
     
-    for linha in arquivo:
-        respostas.append(linha.strip())
-        
-    arquivo.close
     
-    numero = random.randrange(0, len(respostas))
-    palavra_secreta = respostas[numero].upper()
-   
-
-    arquivo2 = open("perguntas.txt", "r")
-    perguntas = []
-
-    for linha in arquivo2:
-        perguntas.append(linha.strip())
-
-    arquivo2.close
-
-    numero2 = random.randrange(0, len(perguntas))
-    pergunta_secreta = perguntas[numero2].upper()
-
-
-
-    print(["_" for letra in palavra_secreta])
+    respostas = ["Ácido carboxílico", "Cetona", "Aldeido", "Éster", "Éter"]
+    
+    resposta1 = [perguntas,0] == [respostas,0]
+    resposta2 = [perguntas,1] == [respostas,1]
+    resposta3 = [perguntas,2] == [respostas,2]
+    resposta4 = [perguntas,3] == [respostas,3]
+    resposta5 = [perguntas,4] == [respostas,4]
+    
     
     
     print("Selecione um nível de dificuldade: ")
@@ -45,16 +34,23 @@ import random
     for rodada in range(1, total_de_tentativas + 1):
 
         print(pergunta_secreta)
+        print(f"Alternativas: {respostas}")
         print(f"Tentativa {rodada} de {total_de_tentativas}")
-        chute = int(input("Insira a resposta: "))
+        chute = input("Insira a resposta: ")
         print("Resposta insetida: ")
         
-        acertou = chute == palavra_secreta
-        errou = chute !== palavra_secreta
         
-        if(acertou):
+        if(chute == resposta1):
             print("Respostas correta")
-        if(errou):
+        elif(chute == resposta2):
+            print("Respostas correta")
+        elif(chute == resposta3):
+            print("Respostas correta")
+        elif(chute == resposta4):
+            print("Respostas correta")
+        elif(chute == resposta5):
+            print("Respostas correta")
+        else:
             print("Resposta errada")
          
         
