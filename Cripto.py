025d73df8af63afee814511b2ea164e79d0ca01f
Matheus Kikumori Almeida Leite <74317831+Kiku2004@users.noreply.github.com>
2021-11-03 13:40:38 -0300

    Update Cripto.py

--- Cripto.py
+++ Cripto.py
@@ -1,1 +1,39 @@
+import random
 
+
+arquivo = open("senhas.txt", "r")
+senhas = []
+
+    for linha in arquio:
+        senhas.append(linha.strip())
+      
+    arquivo.close
+    
+    numero = random.randrange(0, len(senhas))
+    palavra_secreta = senhas[numero].upper()
+    
+    
+    total_de_tentativas = False
+    acertou = False
+    errou = 0
+    palavra_acertada = ["_" for letra in palavra_secreta]
+    
+    print(palavra_acertada)
+    
+    while(not total_de_tentativas and not acertou):
+      
+      chute = input("Digite uma letra:")
+        chute = chute.strip().upper()
+
+        if(chute in palavra_secreta):
+            sequencia = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    palavra_acertada[sequencia] = letra
+                sequencia += 1
+        else:
+            errou += 1
+
+        
+        total_de_tentativas = errou == len(palavra_secreta)
+        acertou = "_" not in palavra acertada
