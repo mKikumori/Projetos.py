895f85ab829106412cbf072443d2ca4ade9cf54e
Matheus Kikumori Almeida Leite <74317831+Kiku2004@users.noreply.github.com>
2021-11-09 16:02:24 -0300

    Update Cripto.py

--- Cripto.py
+++ Cripto.py
@@ -1,18 +1,28 @@
 
 senha_escolhida = input("Crie sua senha:")
 
-total_de_tentativas = 5
 locked = False
 acertou = False
-errou = 0
 palavra_acertada = ["_" for caractere in senha_escolhida]
 
+print("Escolha o nivel de encriptacao")
+print("(1) Fácil, (2) Médio, (3) Difícil")
+numero_de_tentativas = int(input("Nivel: ")
+
+if(nivel == 1):
+    total_de_tentativas = 15
+elif(nivel == 2):
+    total_de_tentativas = 10
+elif(nivel == 3):
+    total_de_tentativas = 5
+
+                           
 print(palavra_acertada)
 
 while (not locked and not acertou):
 
     chute = str(input("Digite uma letra ou numero: "))
-    chute = chute.strip().upper()
+    chute = chute.upper()
 
     if (chute in senha_escolhida):
         sequencia = 0
@@ -21,9 +31,9 @@ while (not locked and not acertou):
                 palavra_acertada[sequencia] = caractere
             sequencia += 1
     else:
-        errou -= 1
+        total_de_tentativas -= 1
 
-    locked = errou == total_de_tentativas == 0
+    locked = total_de_tentativas == 0
     acertou = "_" not in palavra_acertada
     print(palavra_acertada)
 
