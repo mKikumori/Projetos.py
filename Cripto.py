846cba33cde86cf905f7107c9de31375d541945e
Matheus Kikumori Almeida Leite <74317831+Kiku2004@users.noreply.github.com>
2021-11-09 16:05:57 -0300

    Update Cripto.py

--- Cripto.py
+++ Cripto.py
@@ -9,11 +9,11 @@ print("Escolha o nivel de encriptacao")
 print("(1) Fácil, (2) Médio, (3) Difícil")
 numero_de_tentativas = int(input("Nivel: ")
 
-if(nivel == 1):
+if(numero_de_tentativas == 1):
     total_de_tentativas = 15
-elif(nivel == 2):
+elif(numero_de_tentativas == 2):
     total_de_tentativas = 10
-elif(nivel == 3):
+elif(numero_de_tentatuvas == 3):
     total_de_tentativas = 5
 
                            
