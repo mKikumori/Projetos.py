08b0d7eded133c4d5a6e98d9453b8d5d331ea482
Matheus Kikumori Almeida Leite <74317831+Kiku2004@users.noreply.github.com>
2021-11-10 13:45:10 -0300

    Update Cripto.py

--- Cripto.py
+++ Cripto.py
@@ -2,20 +2,21 @@
 senha_escolhida = input("Crie sua senha:")
 
 locked = False
+time_out = False
 acertou = False
 palavra_acertada = ["_" for caractere in senha_escolhida]
+errou = 0
 
 print("Escolha o nivel de encriptacao")
 print("(1) Fácil, (2) Médio, (3) Difícil")
 numero_de_tentativas = int(input("Nivel: ")
 
 if(numero_de_tentativas == 1):
-    total_de_tentativas = 15
-elif(numero_de_tentativas == 2):
-    total_de_tentativas = 10
-elif(numero_de_tentatuvas == 3):
     total_de_tentativas = 5
-
+elif(numero_de_tentativas == 2):
+    total_de_tentativas = 3
+elif(numero_de_tentatuvas == 3):
+    total_de_tentativas = 1
                            
 print(palavra_acertada)
 
@@ -31,11 +32,16 @@ while (not locked and not acertou):
                 palavra_acertada[sequencia] = caractere
             sequencia += 1
     else:
-        total_de_tentativas -= 1
+        errou += 1
 
-    locked = total_de_tentativas == 0
+    locked = errou == len(senha_escolhida)
     acertou = "_" not in palavra_acertada
     print(palavra_acertada)
 
-if (palavra_acertada == senha_escolhida):
-    print(senha_escolhida)
+                           
+    if (locked = True):
+        total_de_tentativas -= 1
+            if (total_de_tentativas == 0):
+                print("Timed-out")
+                           
+print("Fim")
