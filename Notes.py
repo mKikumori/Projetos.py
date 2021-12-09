f220c1cba10ce0e94e4d91364f903e7861c62d09
Matheus Kikumori Almeida Leite <74317831+Kiku2004@users.noreply.github.com>
2021-12-09 16:51:55 -0300

    Update Notes.py

--- Notes.py
+++ Notes.py
@@ -1,1 +1,24 @@
 
+class Notes:
+    
+    def __int__(self, name, date, notes):
+        self.__name = name
+        self.__date = date
+        self.notes = notes
+        
+    @property
+    def name(self):
+        return self.name
+
+    @name.setter
+    def name(self, new_name):
+        self.__name = new_name.title()
+
+    def write_notes(self, text):
+        self.notes += text
+        
+    def read_notes(self):
+        print(self.notes)
+
+    def delete_notes(self, text):
+        self.notes -= text
